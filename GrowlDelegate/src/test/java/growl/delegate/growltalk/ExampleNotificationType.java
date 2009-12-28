a68716692823f1cb89afd2fe2d5ee05991aead25
witspirit <witspirit@072a8d97-7338-0410-a40e-cf6897baf8d7>
2009-12-28 17:22:22 +0000

    - Set pom compiler source/target configuration for JDK 6 - Rename TestNotificationType to ExampleNotificationType to avoid inclusion in the standard maven test configuration - Set source encoding to UTF-8 in pom

--- GrowlDelegate/src/test/java/growl/delegate/growltalk/ExampleNotificationType.java
+++ GrowlDelegate/src/test/java/growl/delegate/growltalk/ExampleNotificationType.java
@@ -1,13 +1,13 @@
 package growl.delegate.growltalk;
 
-public enum TestNotificationType implements NotificationType {
+public enum ExampleNotificationType implements NotificationType {
 	BASIC("Basic Notification", true),
 	OTHER("Other Notification", true);
 	
 	private String name;
 	private boolean enabledByDefault;
 	
-	private TestNotificationType(String name, boolean enabledByDefault) {
+	private ExampleNotificationType(String name, boolean enabledByDefault) {
 		this.name = name;
 		this.enabledByDefault = enabledByDefault;
 	}
