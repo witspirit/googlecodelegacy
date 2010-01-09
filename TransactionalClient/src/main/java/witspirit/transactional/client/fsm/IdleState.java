d19a2a5a31f0248d3d66ab19070a27d64a427f62
witspirit <witspirit@072a8d97-7338-0410-a40e-cf6897baf8d7>
2010-01-09 22:48:15 +0000

    Addition of logging to TransactionalClient

---
+++ TransactionalClient/src/main/java/witspirit/transactional/client/fsm/IdleState.java
@@ -0,0 +1,17 @@
+package witspirit.transactional.client.fsm;
+
+public class IdleState implements TransactionState {
+
+    @Override
+    public TransactionState abort() {
+	// TODO Auto-generated method stub
+	return this;
+    }
+
+    @Override
+    public TransactionState activate() {
+	// TODO Auto-generated method stub
+	return this;
+    }
+
+}
