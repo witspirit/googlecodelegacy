c084e1bb588cb734bc2d1dee83f65457e44d91f2
witspirit <witspirit@072a8d97-7338-0410-a40e-cf6897baf8d7>
2010-01-10 15:04:32 +0000

    - Extracted TransactionRecorder from RequestHandler - Improved testing

--- TransactionalClient/src/main/java/witspirit/transactional/client/Configuration.java
+++ TransactionalClient/src/main/java/witspirit/transactional/client/Configuration.java
@@ -6,18 +6,23 @@ import java.util.List;
 
 public class Configuration<REQUEST> {
     private RequestHandler<REQUEST> requestHandler;
+    private TransactionRecorder<REQUEST> transactionRecorder;
     private List<Integer> repeatIntervalsInSeconds = new ArrayList<Integer>();
     private List<Integer> reversalIntervalsInSeconds = new ArrayList<Integer>();
 
-    public Configuration(RequestHandler<REQUEST> requestHandler) {
-	this(requestHandler, Arrays.asList(0, 0, 0), Arrays.asList(60, 5 * 60, 60 * 60, 2 * 60 * 60));
+    public Configuration(RequestHandler<REQUEST> requestHandler, TransactionRecorder<REQUEST> transactionRecorder) {
+	this(requestHandler, transactionRecorder, Arrays.asList(0, 0, 0), Arrays.asList(60, 5 * 60, 60 * 60, 2 * 60 * 60));
     }
 
-    public Configuration(RequestHandler<REQUEST> requestHandler, List<Integer> repeatIntervalsInSeconds, List<Integer> reversalIntervalsInSeconds) {
+    public Configuration(RequestHandler<REQUEST> requestHandler, TransactionRecorder<REQUEST> transactionRecorder, List<Integer> repeatIntervalsInSeconds, List<Integer> reversalIntervalsInSeconds) {
 	if (requestHandler == null) {
 	    throw new NullPointerException("RequestHandler is mandatory !");
 	}
 	this.requestHandler = requestHandler;
+	if (transactionRecorder == null) {
+	    throw new NullPointerException("TransactionRecorder is mandatory !");
+	}
+	this.transactionRecorder = transactionRecorder;
 	if (repeatIntervalsInSeconds != null) {
 	    this.repeatIntervalsInSeconds.addAll(repeatIntervalsInSeconds);
 	}
@@ -48,5 +53,9 @@ public class Configuration<REQUEST> {
 
     public RequestHandler<REQUEST> getRequestHandler() {
 	return requestHandler;
+    }
+    
+    public TransactionRecorder<REQUEST> getTransactionRecorder() {
+	return transactionRecorder;
     }
 }
--- TransactionalClient/src/test/java/witspirit/transactional/client/ConfigurationTest.java
+++ TransactionalClient/src/test/java/witspirit/transactional/client/ConfigurationTest.java
@@ -12,9 +12,11 @@ public class ConfigurationTest {
     @Test
     public void defaultConfiguration() {
 	RequestHandler<Object> requestHandler = new DummyRequestHandler();
-	Configuration<Object> configuration = new Configuration<Object>(requestHandler);
+	TransactionRecorder<Object> transactionRecorder = new DummyTransactionRecorder();
+	Configuration<Object> configuration = new Configuration<Object>(requestHandler, transactionRecorder);
 	
 	Assert.assertEquals(requestHandler, configuration.getRequestHandler());
+	Assert.assertEquals(transactionRecorder, configuration.getTransactionRecorder());
 	Assert.assertEquals(3, configuration.getMaximumRepeatAttempts());
 	Assert.assertEquals(4, configuration.getMaximumReversalAttempts());
     }
@@ -22,9 +24,11 @@ public class ConfigurationTest {
     @Test
     public void customInitializedConfiguration() {
 	RequestHandler<Object> requestHandler = new DummyRequestHandler();
-	Configuration<Object> configuration = new Configuration<Object>(requestHandler, Arrays.asList(1,2,3,4,5), Arrays.asList(60));
+	TransactionRecorder<Object> transactionRecorder = new DummyTransactionRecorder();
+	Configuration<Object> configuration = new Configuration<Object>(requestHandler, transactionRecorder, Arrays.asList(1,2,3,4,5), Arrays.asList(60));
 	
 	Assert.assertEquals(requestHandler, configuration.getRequestHandler());
+	Assert.assertEquals(transactionRecorder, configuration.getTransactionRecorder());
 	Assert.assertEquals(5, configuration.getMaximumRepeatAttempts());
 	Assert.assertEquals(1, configuration.getMaximumReversalAttempts());
     }
@@ -32,30 +36,41 @@ public class ConfigurationTest {
     @Test
     public void customConfiguration() {
 	RequestHandler<Object> requestHandler = new DummyRequestHandler();
-	Configuration<Object> configuration = new Configuration<Object>(requestHandler);
+	TransactionRecorder<Object> transactionRecorder = new DummyTransactionRecorder();
+	Configuration<Object> configuration = new Configuration<Object>(requestHandler, transactionRecorder);
 	configuration.setRepeatIntervals(1,2,3,4,5).setReversalIntervals(60);
 	
 	Assert.assertEquals(requestHandler, configuration.getRequestHandler());
+	Assert.assertEquals(transactionRecorder, configuration.getTransactionRecorder());
 	Assert.assertEquals(5, configuration.getMaximumRepeatAttempts());
 	Assert.assertEquals(1, configuration.getMaximumReversalAttempts());
     }
     
     @Test
     public void protectionAgainstNulls() {
+	DummyRequestHandler requestHandler = new DummyRequestHandler();
+	TransactionRecorder<Object> transactionRecorder = new DummyTransactionRecorder();
+
 	Configuration<Object> configuration;
 	try {
-	    configuration = new Configuration<Object>(null);
+	    configuration = new Configuration<Object>(null, null);
 	    Assert.fail("Expected NullPointerException for missing RequestHandler");
 	} catch (NullPointerException e) {
 	    Assert.assertTrue(e.getMessage().contains("RequestHandler"));
 	}
 	
-	DummyRequestHandler requestHandler = new DummyRequestHandler();
-	configuration = new Configuration<Object>(requestHandler, null, null);
+	try {
+	    configuration = new Configuration<Object>(requestHandler, null);
+	    Assert.fail("Expected NullPointerException for missing TransactionRecorder");
+	} catch (NullPointerException e) {
+	    Assert.assertTrue(e.getMessage().contains("TransactionRecorder"));
+	}
+	
+	configuration = new Configuration<Object>(requestHandler, transactionRecorder, null, null);
 	Assert.assertEquals(0, configuration.getMaximumRepeatAttempts());
 	Assert.assertEquals(0, configuration.getMaximumReversalAttempts());
 	
-	configuration = new Configuration<Object>(requestHandler);
+	configuration = new Configuration<Object>(requestHandler, transactionRecorder);
 	configuration.setRepeatIntervals();
 	configuration.setReversalIntervals();
 	Assert.assertEquals(0, configuration.getMaximumRepeatAttempts());
@@ -73,6 +88,10 @@ public class ConfigurationTest {
 	public void doSendReversal(Object request, String transactionId) throws TimeoutException {
 	    
 	}
+	
+    }
+    
+    private class DummyTransactionRecorder implements TransactionRecorder<Object> {
 
 	@Override
 	public void transactionDone(Object request, String transactionId, TransactionStatus status) {
