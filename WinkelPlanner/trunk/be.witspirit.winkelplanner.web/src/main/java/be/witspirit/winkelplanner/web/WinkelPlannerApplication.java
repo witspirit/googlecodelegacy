378094c06dd2ab4f5f750647dcfbf3a02b5f8063
witspirit <witspirit@072a8d97-7338-0410-a40e-cf6897baf8d7>
2010-12-26 18:57:45 +0000

    Working on the skeleton for WinkelPlanner. Introduced a free CSS from the web to get me going in a more or less acceptable style.

--- WinkelPlanner/trunk/be.witspirit.winkelplanner.web/src/main/java/be/witspirit/winkelplanner/web/WinkelPlannerApplication.java
+++ WinkelPlanner/trunk/be.witspirit.winkelplanner.web/src/main/java/be/witspirit/winkelplanner/web/WinkelPlannerApplication.java
@@ -1,11 +1,13 @@
-package be.witspirit.winkelplanner;
+package be.witspirit.winkelplanner.web;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.HttpSessionStore;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.session.ISessionStore;
 
-public class GaeWicketApplication extends WebApplication
+import be.witspirit.winkelplanner.HomePage;
+
+public class WinkelPlannerApplication extends WebApplication
 {
     @Override
     public Class<? extends Page> getHomePage() {
---
+++ WinkelPlanner/trunk/be.witspirit.winkelplanner.web/src/main/java/be/witspirit/winkelplanner/web/common/BasePage.java
@@ -0,0 +1,7 @@
+package be.witspirit.winkelplanner.web.common;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class BasePage extends WebPage {
+
+}
